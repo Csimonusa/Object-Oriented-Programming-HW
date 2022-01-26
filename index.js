2a0b4cc1815109fcdc0d69b865f1b5e92fb4f4ec
mona <csimonusa@outlook.com>
2022-01-25 21:02:11 -0500

    added callback to execute when the then promise is fulfilled for ea prompt function

--- index.js
+++ index.js
@@ -10,83 +10,95 @@ menu = () => {
         inquirer
             .prompt([
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Name of manager?',
+                    name: 'name'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Employee id of manager?',
+                    name: 'employeeId'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Email of manager?',
+                    name: 'email'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Address of manager?',
+                    name: 'address'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Office number of manager?',
+                    name: 'officeNumber'
                 }
             ])
+            .then(({name, employeeId, email, address, officeNumber}) => {
+                const manager = new Manager(name, employeeId, email, address, officeNumber)
+                console.log(manager)
+            })
     }
 
     createIntern = () => {
         inquirer
             .prompt([
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Name of intern?',
+                    name: 'name'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Employee id of intern?',
+                    name: 'employeeId'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Email of intern?',
+                    name: 'email'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'School of intern?',
+                    name: 'school'
                 },
             ])
+            .then(({name, employeeId, email, school}) => {
+                const intern = new Intern(name, employeeId, email, school)
+                console.log(intern)
+            })
     }
 
     createEngineer = () => {
         inquirer
             .prompt([
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Name of engineer?',
+                    name: 'name'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Employee id of engineer?',
+                    name: 'employeeId'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Email of engineer?',
+                    name: 'email'
                 },
                 {
-                    type: '',
-                    message: '',
-                    name: ''
+                    type: 'input',
+                    message: 'Github username of engineer?',
+                    name: 'github'
                 },
             ])
+            .then(({name, employeeId, email, github}) => {
+                const engineer = new Engineer(name, employeeId, email, github)
+                console.log(engineer)
+            })
     }
 
 }
